2e942620175584bc31a0e89a2831ef6b5928b708
Addie Abukasis <addieabuka@gmail.com>
2019-10-17 19:44:44 -0700

    Uploading Website to Github

---
+++ SmartAppWeb/SmartAppWeb/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SmartAppWeb.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading;
+
+namespace SmartAppWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public ActionResult SendEmail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact( string subject, string message)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    string receiver = "addieabuka@gmail.com";
+                    var senderEmail = new MailAddress("smartappsoftwarecontact@gmail.com", "Addie");
+                    var receiverEmail = new MailAddress(receiver, "Receiver");
+                    var password = "smartapp123";
+                    var sub = subject;
+                    var body = message;
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(senderEmail.Address, password)
+                    };
+                    using (var mess = new MailMessage(senderEmail, receiverEmail)
+                    {
+                        Subject = subject,
+                        Body = body
+                    })
+                    {
+                        smtp.Send(mess);
+                    }
+                    return View();
+                }
+            }
+            catch (Exception)
+            {
+                ViewBag.Error = "Some Error";
+            }
+            return View();
+        }
+    }
+}
---
+++ SmartAppWeb/SmartAppWeb/emailcode.cs
@@ -0,0 +1,3 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+
